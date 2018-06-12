5a263f5bb4f7e8f29f64a3d7f29ed5929eff7ac5
chenshuai <chenshuai92@hotmail.com>
2018-06-12 10:40:44 +0800

    custom queue

---
+++ src/com/ofloxacin/CustomQueue.java
@@ -0,0 +1,42 @@
+package com.ofloxacin;
+
+import java.util.Stack;
+
+/**
+ * @author ChenShuai
+ * @date 2018/6/12 9:26
+ */
+public class CustomQueue {
+    private Stack<Object> s1 = new Stack<Object>();
+    private Stack<Object> s2 = new Stack<Object>();
+
+    public void add(Object o) {
+        if (null != o) {
+            s1.push(o);
+        }
+    }
+
+    public Object pull() {
+        if (s2.size() == 0) {
+            while (s1.size() > 0) {
+                s2.push(s1.pop());
+            }
+        }
+
+        return s2.size() == 0 ? null : s2.pop();
+    }
+
+    public Object peek() {
+        if (s2.size() == 0) {
+            while (s1.size() > 0) {
+                s2.push(s1.pop());
+            }
+        }
+
+        return s2.size() == 0 ? null : s2.peek();
+    }
+
+    public int size() {
+        return s1.size() + s2.size();
+    }
+}
--- src/com/ofloxacin/Main.java
+++ src/com/ofloxacin/Main.java
@@ -1,11 +1,49 @@
 package com.ofloxacin;
 
+import java.util.Random;
+import java.util.concurrent.*;
+
 /**
  * @author ChenShuai
  * @date 2018/6/12 9:22
  */
 public class Main {
     public static void main(String[] args) {
+        final CustomQueue customQueue = new CustomQueue();
+        final int MAX_COUNT = 100;
 
+        BlockingQueue<Runnable> workingQueue = new ArrayBlockingQueue<Runnable>(10);
+
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(2 ,5, 100, TimeUnit.SECONDS, workingQueue);
+        threadPoolExecutor.prestartAllCoreThreads();
+
+        workingQueue.add(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName());
+                Random random = new Random();
+                for (int i = 0; i < MAX_COUNT; i++) {
+                    customQueue.add(i);
+                    try {
+                        Thread.sleep(random.nextInt(100));
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        workingQueue.add(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName());
+                while (true) {
+                    while (customQueue.size() > 0) {
+                        System.out.println(customQueue.pull());
+                    }
+                    Thread.yield();
+                }
+            }
+        });
     }
 }
