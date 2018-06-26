b8b4d4e6eb1b4ccbc5dea1c6e18a6faf800aa160
chenshuai <chenshuai92@hotmail.com>
2018-06-26 18:35:20 +0800

    add thread test

---
+++ src/com/ofloxacin/ThreadTest.java
@@ -0,0 +1,19 @@
+package com.ofloxacin;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadInfo;
+import java.lang.management.ThreadMXBean;
+
+/**
+ * @author ChenShuai
+ * @date 2018/6/26 18:16
+ */
+public class ThreadTest {
+    public static void main(String[] args) {
+        ThreadMXBean threadMXBean = ManagementFactory.getThreadMXBean();
+        ThreadInfo[] threadInfos = threadMXBean.dumpAllThreads(false, false);
+        for (ThreadInfo threadInfo : threadInfos) {
+            System.out.println("[" + threadInfo.getThreadId() + "] " + threadInfo.getThreadName());
+        }
+    }
+}
