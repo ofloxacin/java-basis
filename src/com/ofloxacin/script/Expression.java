f8c84b9ec6b89837b1c9b75b59879eec52527eeb
chenshuai <chenshuai92@hotmail.com>
2018-07-12 17:23:16 +0800

    add script test

---
+++ src/com/ofloxacin/script/Expression.java
@@ -0,0 +1,27 @@
+package com.ofloxacin.script;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author ChenShuai
+ * @date 2018/7/12 15:58
+ */
+public class Expression {
+    public static class Node {
+        public String code;
+        public String name;
+        public String value;
+        public String exp;
+        public List<Expression.Node> subExp;
+
+        public Node(String code, String name, String value, String exp, List<Expression.Node> subExp) {
+            this.code = code;
+            this.name = name;
+            this.value = value;
+            this.exp = exp;
+            this.subExp = subExp;
+        }
+    }
+}
---
+++ src/com/ofloxacin/script/ScriptTest.java
@@ -0,0 +1,60 @@
+package com.ofloxacin.script;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+import java.util.*;
+
+/**
+ * @author ChenShuai
+ * @date 2018/7/9 17:45
+ */
+public class ScriptTest {
+    private static ScriptEngine jse = new ScriptEngineManager().getEngineByName("JavaScript");
+
+    public static void main(String[] args) {
+        Expression.Node length = new Expression.Node("length", "长", "300", null, null);
+        Expression.Node width = new Expression.Node("width", "宽", "200", null, null);
+        Expression.Node height = new Expression.Node("height", "高", "100", null, null);
+        Expression.Node bolang = new Expression.Node("bolang", "波浪刀", null, "$[length]+$[width]", Arrays.asList(length, width));
+        Expression.Node zhidao = new Expression.Node("zhidao", "直刀", null, "$[length]*$[height]", Arrays.asList(length, height));
+        Expression.Node node = new Expression.Node("knife_fee", "刀版费", null, "$[bolang]+$[zhidao]", Arrays.asList(bolang, zhidao));
+
+        Set<Expression.Node> result = new HashSet<>();
+        System.out.println(eval(node, result));
+        System.out.println();
+    }
+
+    public static String eval(Expression.Node node, Set<Expression.Node> result) {
+        if (result.contains(node)) {
+            return node.value;
+        }
+
+        if (node.subExp == null) {
+            result.add(node);
+            return node.value;
+        }
+
+        for (Expression.Node node1 : node.subExp) {
+            node1.value = eval(node1, result);
+        }
+
+        String newExp = replaceSubExp(node);
+
+        try {
+            node.value = jse.eval(newExp).toString();
+        } catch (ScriptException e) {
+            e.printStackTrace();
+        }
+
+        return node.value;
+    }
+
+    public static String replaceSubExp(Expression.Node node) {
+        String exp = node.exp;
+        for (Expression.Node subNode : node.subExp) {
+            exp = exp.replace("$[" + subNode.code + "]", subNode.value);
+        }
+        return exp;
+    }
+}
