a65ea32fbadb1fe08fb8801faaeb58569dad0156
ShuaiChen <chenshuai92@hotmail.com>
2018-11-28 18:17:14 +0800

    add file-system test

---
+++ src/main/java/com/ofloxacin/corejavaii/ZipTest.java
@@ -0,0 +1,33 @@
+package com.ofloxacin.corejavaii;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
+/**
+ * @author chens
+ * @date 2018/11/28 17:57
+ */
+public class ZipTest {
+    public static void main(String[] args) {
+        try {
+            FileSystem fileSystem = FileSystems.newFileSystem(Paths.get("D:\\_Setup\\Development\\apache-tomcat-8.5.32.zip"), null);
+            Files.walkFileTree(fileSystem.getPath("/"), new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                    System.out.println(dir.toAbsolutePath());
+                    return super.preVisitDirectory(dir, attrs);
+                }
+
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    System.out.println(file.toAbsolutePath());
+                    return super.visitFile(file, attrs);
+                }
+            });
+            fileSystem.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
