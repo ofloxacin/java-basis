ab6e0f99ea8fb734e4a47c5423515bbf05d30551
Shuai Chen <chenshuai92@hotmail.com>
2019-02-21 18:55:07 +0800

    add sll test

---
+++ src/main/java/com/ofloxacin/SSLTest.java
@@ -0,0 +1,24 @@
+package com.ofloxacin;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Scanner;
+
+/**
+ * @author chens
+ * @date 2019/2/21 14:38
+ */
+public class SSLTest {
+    public static void main(String[] args) throws IOException {
+        final String url = "https://www.baidu.com/";
+        URL realUrl = new URL(url);
+        URLConnection connection = realUrl.openConnection();
+        InputStream inputStream = connection.getInputStream();
+        Scanner scanner = new Scanner(inputStream);
+        while (scanner.hasNextLine()) {
+            System.out.println(scanner.nextLine());
+        }
+    }
+}
---
+++ src/main/java/com/ofloxacin/Test.java
@@ -0,0 +1,11 @@
+package com.ofloxacin;
+
+/**
+ * @author chens
+ * @date 2018/12/6 20:24
+ */
+public class Test {
+    public static void main(String[] args) {
+        
+    }
+}
