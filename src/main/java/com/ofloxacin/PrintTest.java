fc9738bd96871a4759ced6badb53ccc22af0594e
Shuai Chen <chenshuai92@hotmail.com>
2019-04-11 20:58:55 +0800

    add print test

---
+++ src/main/java/com/ofloxacin/PrintTest.java
@@ -0,0 +1,16 @@
+package com.ofloxacin;
+
+/**
+ * @author chens
+ * @date 2019/4/11 20:54
+ */
+public class PrintTest {
+    public static void main(String[] args) throws InterruptedException {
+        int MAX = 1000000000;
+        int i = 0;
+        while (i < MAX) {
+            Thread.sleep(100);
+            System.out.print(++i + "\r");
+        }
+    }
+}
