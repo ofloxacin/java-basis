5e829d5aebd6d678c456a2d70e09d17932c5ddfa
Shuai Chen <chenshuai92@hotmail.com>
2019-10-31 21:26:28 +0800

    P002_AddTwoNumbers

---
+++ src/main/java/com/ofloxacin/leetcode/P002_AddTwoNumbers.java
@@ -0,0 +1,65 @@
+package com.ofloxacin.leetcode;
+
+/**
+ * @author chenshuai
+ * @version 0.1
+ * @name P002_AddTwoNumbers
+ * @description
+ * @date 2019/10/31
+ */
+public class P002_AddTwoNumbers {
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(2);
+        ListNode l2 = new ListNode(9);
+        l2.next = new ListNode(8);
+        ListNode listNode = addTwoNumbers(l1, l2);
+        while (listNode != null) {
+            System.out.print(listNode.val);
+            listNode = listNode.next;
+        }
+    }
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode root = new ListNode(0);
+        ListNode result = root;
+        ListNode p1 = l1, p2 = l2;
+        int increase = 0;
+        while (p1 != null || p2 != null) {
+            int x = 0, y = 0;
+            if (p1 != null) {
+                x = p1.val;
+                p1 = p1.next;
+            }
+            if (p2 != null) {
+                y = p2.val;
+                p2 = p2.next;
+            }
+            int temp = x + y + increase;
+            if (temp < 10) {
+                result.next = new ListNode(temp);
+                result = result.next;
+                increase = 0;
+            } else {
+                result.next = new ListNode(temp - 10);
+                result = result.next;
+                increase = 1;
+            }
+        }
+        if (increase > 0) {
+            result.next = new ListNode(increase);
+        }
+        return root.next;
+    }
+
+    public static class ListNode {
+
+        int val;
+
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
