86e72fe526a9318928dd0c267ac11a061a58e266
chenshuai <chenshuai92@hotmail.com>
2020-05-12 00:09:18 +0800

    add rotate

--- src/main/java/com/ofloxacin/leetcode/interview/array/P003_Rotate.java
+++ src/main/java/com/ofloxacin/leetcode/interview/array/P003_Rotate.java
@@ -28,10 +28,29 @@ public class P003_Rotate {
 
     public void rotate(int[] nums, int k) {
         k = k % nums.length;
-        for (int i = 0; i < nums.length; i++) {
-            int n = nums[i];
-            nums[i] = nums[(i + k) % nums.length];
-            nums[(i + k) % nums.length] = n;
+        int count = 0;
+        for (int start = 0; count < nums.length; start++) {
+            int current = start;
+            int prev = nums[start];
+            do {
+                int next = (current + k) % nums.length;
+                int temp = nums[next];
+                nums[next] = prev;
+                prev = temp;
+                current = next;
+                count++;
+            } while (start != current);
+        }
+    }
+
+    public void rotate2(int[] nums, int k) {
+        k = k % nums.length;
+        for (int i = 0; i < k; i++) {
+            int v = nums[nums.length - 1];
+            for (int j = nums.length - 1; j > 0; j--) {
+                nums[j] = nums[j - 1];
+            }
+            nums[0] = v;
         }
     }
 }
