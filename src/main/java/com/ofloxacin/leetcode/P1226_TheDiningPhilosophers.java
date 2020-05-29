d5a2d4ab6a40bb4d1dc885c9d4b0791e894a7815
Shuai Chen <chenshuai92@hotmail.com>
2020-05-29 10:35:07 +0800

    add philosophers

--- src/main/java/com/ofloxacin/leetcode/P1226_TheDiningPhilosophers.java
+++ src/main/java/com/ofloxacin/leetcode/P1226_TheDiningPhilosophers.java
@@ -15,19 +15,17 @@ public class P1226_TheDiningPhilosophers {
 
     @Test
     public void test() {
-        Solution solution = new Solution1();
+        Solution solution = new Solution2();
         for (int i = 0; i < 5; i++) {
             int philosopher = i;
             new Thread(() -> {
                 try {
-                    while (true) {
-                        solution.wantsToEat(philosopher,
-                                () -> System.out.println(Arrays.asList(philosopher, 1, 1)),
-                                () -> System.out.println(Arrays.asList(philosopher, 2, 1)),
-                                () -> System.out.println(Arrays.asList(philosopher, 0, 3)),
-                                () -> System.out.println(Arrays.asList(philosopher, 1, 2)),
-                                () -> System.out.println(Arrays.asList(philosopher, 2, 2)));
-                    }
+                    solution.wantsToEat(philosopher,
+                            () -> System.out.println(Arrays.asList(philosopher, 1, 1)),
+                            () -> System.out.println(Arrays.asList(philosopher, 2, 1)),
+                            () -> System.out.println(Arrays.asList(philosopher, 0, 3)),
+                            () -> System.out.println(Arrays.asList(philosopher, 1, 2)),
+                            () -> System.out.println(Arrays.asList(philosopher, 2, 2)));
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -46,9 +44,10 @@ public class P1226_TheDiningPhilosophers {
                         Runnable putRightFork) throws InterruptedException;
     }
 
+    /**
+     * 限定就餐策略
+     */
     class Solution1 implements Solution {
-
-        private boolean[] ates = new boolean[5];
 
         private Semaphore[] semaphores = new Semaphore[]{
                 new Semaphore(1),
@@ -64,19 +63,98 @@ public class P1226_TheDiningPhilosophers {
                                Runnable eat,
                                Runnable putLeftFork,
                                Runnable putRightFork) throws InterruptedException {
-            if (ates[philosopher]) {
-                return;
+            int left = philosopher;
+            int right = (philosopher + 1) % 5;
+            if ((philosopher & 1) == 0) {
+                semaphores[left].acquire();
+                semaphores[right].acquire();
+            } else {
+                semaphores[right].acquire();
+                semaphores[left].acquire();
             }
-            semaphores[philosopher].acquire();
             pickLeftFork.run();
-            semaphores[(philosopher + 1) % 5].acquire();
             pickRightFork.run();
             eat.run();
-            semaphores[philosopher].release();
             putLeftFork.run();
-            semaphores[(philosopher + 1) % 5].release();
             putRightFork.run();
-            ates[philosopher] = true;
+            semaphores[left].release();
+            semaphores[right].release();
+        }
+    }
+
+    /**
+     * 限定就餐数量
+     */
+    class Solution2 implements Solution {
+
+        private Semaphore[] semaphores = new Semaphore[]{
+                new Semaphore(1),
+                new Semaphore(1),
+                new Semaphore(1),
+                new Semaphore(1),
+                new Semaphore(1)
+        };
+
+        private Semaphore available = new Semaphore(4);
+
+        @Override
+        public void wantsToEat(int philosopher,
+                               Runnable pickLeftFork,
+                               Runnable pickRightFork,
+                               Runnable eat,
+                               Runnable putLeftFork,
+                               Runnable putRightFork) throws InterruptedException {
+            available.acquire();
+            int left = philosopher;
+            int right = (philosopher + 1) % 5;
+            semaphores[left].acquire();
+            semaphores[right].acquire();
+            pickLeftFork.run();
+            pickRightFork.run();
+            eat.run();
+            putLeftFork.run();
+            putRightFork.run();
+            semaphores[left].release();
+            semaphores[right].release();
+            available.release();
+        }
+    }
+
+    /**
+     * 同时拿起左右叉子
+     */
+    class Solution3 implements Solution {
+
+        private Semaphore[] semaphores = new Semaphore[]{
+                new Semaphore(1),
+                new Semaphore(1),
+                new Semaphore(1),
+                new Semaphore(1),
+                new Semaphore(1)
+        };
+
+        private Semaphore lock = new Semaphore(1);
+
+        @Override
+        public void wantsToEat(int philosopher,
+                               Runnable pickLeftFork,
+                               Runnable pickRightFork,
+                               Runnable eat,
+                               Runnable putLeftFork,
+                               Runnable putRightFork) throws InterruptedException {
+            lock.acquire();
+            int left = philosopher;
+            int right = (philosopher + 1) % 5;
+            semaphores[left].acquire();
+            semaphores[right].acquire();
+            pickLeftFork.run();
+            pickRightFork.run();
+            lock.release();
+            eat.run();
+            putLeftFork.run();
+            putRightFork.run();
+            semaphores[left].release();
+            semaphores[right].release();
         }
     }
 }
