f4ed9d11bdf17e13e5132f3dedb31cbb552dd162
Shuai Chen <chenshuai92@hotmail.com>
2020-07-16 18:27:15 +0800

    add test

--- src/main/java/com/ofloxacin/algorithm/sort/MergeSort2.java
+++ src/main/java/com/ofloxacin/algorithm/sort/MergeSort2.java
@@ -1,24 +1,27 @@
 package com.ofloxacin.algorithm.sort;
 
+import com.ofloxacin.util.PrintUtil;
+
 /**
  * @author chenshuai
  * @version 0.1
  * @description
- * @date 2020-05-29
+ * @date 2020-07-16
  */
-public class MergeSort implements Sort {
+public class MergeSort2 implements Sort {
 
     @Override
     public void sort(int[] nums) {
-        mergeSort(nums, 0, nums.length);
+        mergeSort(nums);
     }
 
-    private void mergeSort(int[] nums, int low, int high) {
-        if (high - low < 2) return;
-        int mid = low + (high - low) / 2;
-        mergeSort(nums, low, mid);
-        mergeSort(nums, mid, high);
-        merge(nums, low, mid, high);
+    private void mergeSort(int[] nums) {
+        for (int len = 1; len <= nums.length; len *= 2) {
+            for (int low = 0; low + len <= nums.length; low += len * 2) {
+                int high = Math.min(nums.length, low + len * 2);
+                merge(nums, low, low + (high - low) / 2, high);
+            }
+        }
     }
 
     private void merge(int[] nums, int low, int mid, int high) {
@@ -34,5 +37,11 @@ public class MergeSort implements Sort {
         while (i < mid) result[k++] = nums[i++];
         while (j < high) result[k++] = nums[j++];
         System.arraycopy(result, 0, nums, low, result.length);
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{5, 4, 3, 2, 1, 0};
+        new MergeSort2().sort(nums);
+        PrintUtil.println(nums);
     }
 }
