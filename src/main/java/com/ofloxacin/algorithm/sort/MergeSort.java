9080a2178a45493336a00c1c06a35728b9c0fda1
Shuai Chen <chenshuai92@hotmail.com>
2020-07-16 19:32:44 +0800

    add test

--- src/main/java/com/ofloxacin/algorithm/sort/MergeSort.java
+++ src/main/java/com/ofloxacin/algorithm/sort/MergeSort.java
@@ -1,5 +1,7 @@
 package com.ofloxacin.algorithm.sort;
 
+import com.ofloxacin.util.PrintUtil;
+
 /**
  * @author chenshuai
  * @version 0.1
@@ -10,29 +12,35 @@ public class MergeSort implements Sort {
 
     @Override
     public void sort(int[] nums) {
-        mergeSort(nums, 0, nums.length);
+        mergeSort(nums, 0, nums.length - 1);
     }
 
     private void mergeSort(int[] nums, int low, int high) {
-        if (high - low < 2) return;
+        if (low >= high) return;
         int mid = low + (high - low) / 2;
         mergeSort(nums, low, mid);
-        mergeSort(nums, mid, high);
+        mergeSort(nums, mid + 1, high);
         merge(nums, low, mid, high);
     }
 
     private void merge(int[] nums, int low, int mid, int high) {
-        int[] result = new int[high - low];
-        int i = low, j = mid, k = 0;
-        while (i < mid && j < high) {
+        int[] result = new int[high - low + 1];
+        int i = low, j = mid + 1, k = 0;
+        while (i <= mid && j <= high) {
             if (nums[i] <= nums[j]) {
                 result[k++] = nums[i++];
             } else {
                 result[k++] = nums[j++];
             }
         }
-        while (i < mid) result[k++] = nums[i++];
-        while (j < high) result[k++] = nums[j++];
+        while (i <= mid) result[k++] = nums[i++];
+        while (j <= high) result[k++] = nums[j++];
         System.arraycopy(result, 0, nums, low, result.length);
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{5, 4, 3, 2, 1, 0};
+        new MergeSort().sort(nums);
+        PrintUtil.println(nums);
     }
 }
--- src/main/java/com/ofloxacin/algorithm/sort/MergeSort2.java
+++ src/main/java/com/ofloxacin/algorithm/sort/MergeSort2.java
@@ -18,24 +18,23 @@ public class MergeSort2 implements Sort {
     private void mergeSort(int[] nums) {
         for (int len = 1; len <= nums.length; len *= 2) {
             for (int low = 0; low + len <= nums.length; low += len * 2) {
-                int high = Math.min(nums.length, low + len * 2);
-                merge(nums, low, low + (high - low) / 2, high);
+                merge(nums, low, low + len - 1, Math.min(nums.length - 1, low + len * 2 - 1));
             }
         }
     }
 
     private void merge(int[] nums, int low, int mid, int high) {
-        int[] result = new int[high - low];
-        int i = low, j = mid, k = 0;
-        while (i < mid && j < high) {
+        int[] result = new int[high - low + 1];
+        int i = low, j = mid + 1, k = 0;
+        while (i <= mid && j <= high) {
             if (nums[i] <= nums[j]) {
                 result[k++] = nums[i++];
             } else {
                 result[k++] = nums[j++];
             }
         }
-        while (i < mid) result[k++] = nums[i++];
-        while (j < high) result[k++] = nums[j++];
+        while (i <= mid) result[k++] = nums[i++];
+        while (j <= high) result[k++] = nums[j++];
         System.arraycopy(result, 0, nums, low, result.length);
     }
 
