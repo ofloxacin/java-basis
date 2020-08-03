3965b54d0f9188a744e13be52d5a4c0ccb74aa3f
chenshuai <chenshuai92@hotmail.com>
2020-08-03 23:49:19 +0800

    add merge

--- src/main/java/com/ofloxacin/leetcode/P0021_MergeTwoSortedLists.java
+++ src/main/java/com/ofloxacin/leetcode/P0021_MergeTwoSortedLists.java
@@ -1,116 +1,94 @@
 package com.ofloxacin.leetcode;
 
+import org.junit.jupiter.api.Test;
+
 /**
  * @author chenshuai
  * @version 0.1
- * @name P0021_MergeTwoSortedLists
- * @description
  * @date 2019/11/9
  */
 public class P0021_MergeTwoSortedLists {
 
-    public static void main(String[] args) {
+    interface Solution {
+
+        ListNode mergeTwoLists(ListNode l1, ListNode l2);
+    }
+
+    @Test
+    public void test() {
         ListNode l1 = new ListNode(1);
         l1.next = new ListNode(2);
         l1.next.next = new ListNode(4);
-        ListNode l2 = new ListNode(5);
+        ListNode l2 = new ListNode(1);
         l2.next = new ListNode(3);
         l2.next.next = new ListNode(4);
-        ListNode node = mergeTwoLists2(l1, l2);
+        Solution solution = new Solution3();
+        ListNode node = solution.mergeTwoLists(l1, l2);
         while (node != null) {
             System.out.print(node.val + ",");
             node = node.next;
         }
     }
 
-    public static ListNode mergeTwoLists4(ListNode l1, ListNode l2) {
-        ListNode dummyHead = new ListNode(0);
-        ListNode cur = dummyHead;
-        while (l1 != null && l2 != null) {
-            if (l1.val <= l2.val) {
-                cur.next = l1;
-                l1 = l1.next;
+    class Solution1 implements Solution {
+
+        @Override
+        public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+            ListNode dummyHead = new ListNode(0);
+            ListNode cur = dummyHead;
+            while (l1 != null && l2 != null) {
+                if (l1.val <= l2.val) {
+                    cur.next = l1;
+                    l1 = l1.next;
+                } else {
+                    cur.next = l2;
+                    l2 = l2.next;
+                }
+                cur = cur.next;
+            }
+            cur.next = l1 == null ? l2 : l1;
+            return dummyHead.next;
+        }
+    }
+
+    class Solution2 implements Solution {
+
+        @Override
+        public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+            if (l1 == null) {
+                return l2;
+            } else if (l2 == null) {
+                return l1;
+            } else if (l1.val < l2.val) {
+                l1.next = mergeTwoLists(l1.next, l2);
+                return l1;
             } else {
-                cur.next = l2;
-                l2 = l2.next;
+                l2.next = mergeTwoLists(l1, l2.next);
+                return l2;
             }
-            cur = cur.next;
-        }
-        cur.next = l1 == null ? l2 : l1;
-        return dummyHead.next;
-    }
-
-    public static ListNode mergeTwoLists3(ListNode l1, ListNode l2) {
-        if (l1 == null) {
-            return l2;
-        } else if (l2 == null) {
-            return l1;
-        } else if (l1.val < l2.val) {
-            l1.next = mergeTwoLists3(l1.next, l2);
-            return l1;
-        } else {
-            l2.next = mergeTwoLists3(l1, l2.next);
-            return l2;
         }
     }
 
-    public static ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
-        if (l1 == null) {
-            return l2;
-        }
-        if (l2 == null) {
-            return l1;
-        }
-        ListNode p1 = l1.val <= l2.val ? l1 : l2;
-        ListNode p2 = l1.val > l2.val ? l1 : l2;
-        ListNode root = p1;
-        while (p2 != null) {
-            while (p1.next != null && p1.next.val <= p2.val) {
-                p1 = p1.next;
-            }
-            if (p1.next == null) {
+    class Solution3 implements Solution {
+
+        @Override
+        public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+            if (l1 == null) return l2;
+            if (l2 == null) return l1;
+            ListNode p1 = l1.val <= l2.val ? l1 : l2;
+            ListNode p2 = l1.val <= l2.val ? l2 : l1;
+            ListNode head = p1;
+            while (true) {
+                while (p1.next != null && p1.next.val <= p2.val) p1 = p1.next;
+                if (p1.next == null) {
+                    p1.next = p2;
+                    return head;
+                }
+                ListNode temp = p1.next;
                 p1.next = p2;
-                return root;
+                p2 = temp;
             }
-            ListNode p2s = p2;
-            while (p2.next != null && p2.next.val <= p1.next.val) {
-                p2 = p2.next;
-            }
-            ListNode temp = p1.next;
-            p1.next = p2s;
-            p1 = temp;
-            temp = p2.next;
-            p2.next = p1;
-            p2 = temp;
         }
-        return root;
-    }
-
-    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode root = new ListNode(0);
-        ListNode p = root;
-        ListNode p1 = l1, p2 = l2;
-        while (p1 != null && p2 != null) {
-            if (p1.val <= p2.val) {
-                p.next = new ListNode(p1.val);
-                p1 = p1.next;
-            } else {
-                p.next = new ListNode(p2.val);
-                p2 = p2.next;
-            }
-            p = p.next;
-        }
-        while (p1 != null) {
-            p.next = new ListNode(p1.val);
-            p1 = p1.next;
-            p = p.next;
-        }
-        while (p2 != null) {
-            p.next = new ListNode(p2.val);
-            p2 = p2.next;
-            p = p.next;
-        }
-        return root.next;
     }
 
     public static class ListNode {
@@ -118,6 +96,7 @@ public class P0021_MergeTwoSortedLists {
         int val;
 
         ListNode next;
+
         ListNode(int x) {
             val = x;
         }
