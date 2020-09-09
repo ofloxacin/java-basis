053a58ca7d14fd8bc784a9e5244381b728c68fba
Shuai Chen <chenshuai92@hotmail.com>
2020-09-09 14:54:27 +0800

    add thread

--- src/main/java/com/ofloxacin/interview/quna/Quna.java
+++ src/main/java/com/ofloxacin/interview/quna/Quna.java
@@ -2,8 +2,9 @@ package com.ofloxacin.interview.quna;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Callable;
+import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -21,21 +22,23 @@ public class Quna {
     private static volatile boolean start = false;
 
     public static void main(String[] args) {
+        test2();
+    }
+
+    public static void test1() {
         CountDownLatch latch = new CountDownLatch(4);
-        List<Callable<Integer>> tasks = new ArrayList<>(4);
-        for (int i = 0; i < 4; i++) {
-            tasks.add(() -> {
-                ThreadLocalRandom random = ThreadLocalRandom.current();
-                latch.countDown();
-                while (!start) {
-                }
-                return random.nextInt(10);
-            });
-        }
         ExecutorService threadPool = Executors.newFixedThreadPool(4);
         try {
             List<Future<Integer>> results = new ArrayList<>(4);
-            tasks.forEach(t -> results.add(threadPool.submit(t)));
+            for (int i = 0; i < 4; i++) {
+                results.add(threadPool.submit(() -> {
+                    ThreadLocalRandom random = ThreadLocalRandom.current();
+                    latch.countDown();
+                    while (!start) {
+                    }
+                    return random.nextInt(10);
+                }));
+            }
             latch.await();
             start = true;
             int sum = 0;
@@ -45,8 +48,37 @@ public class Quna {
             }
             System.out.println(sum);
         } catch (InterruptedException | ExecutionException e) {
-
+            e.printStackTrace();
         }
+        threadPool.shutdown();
+    }
+
+    public static void test2() {
+        CyclicBarrier cb = new CyclicBarrier(4);
+        ExecutorService threadPool = Executors.newFixedThreadPool(4);
+        List<Future<Integer>> futures = new ArrayList<>(4);
+        for (int i = 0; i < 4; i++) {
+            Future<Integer> future = threadPool.submit(() -> {
+                ThreadLocalRandom random = ThreadLocalRandom.current();
+                try {
+                    cb.await();
+                } catch (InterruptedException | BrokenBarrierException e) {
+                    e.printStackTrace();
+                }
+                return random.nextInt(10);
+            });
+            futures.add(future);
+        }
+        int sum = 0;
+        for (Future<Integer> future : futures) {
+            try {
+                System.out.println(future.get());
+                sum += future.get();
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(sum);
         threadPool.shutdown();
     }
 }
